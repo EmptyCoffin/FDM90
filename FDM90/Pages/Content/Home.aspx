36398aacda126e67a780647356d8ebb2a6ef1678
owner <owner@HP>
2017-05-07 16:52:03 +0100

    initial markup for setting and displaying goals

--- FDM90/Pages/Content/Home.aspx
+++ FDM90/Pages/Content/Home.aspx
@@ -1,18 +1,69 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Site.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="FDM90.Pages.Content.Home" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-<div>
-    <h3>
-        Home page
-    </h3>
 
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Home page
+    </h3>
     <p>
         About this site!
     </p>
-            <asp:Button id="facebookSetUpButton" visible="false" runat="server" class="btn btn-primary btn-block" Text="Facebook Set Up" OnClick="facebookSetUpButton_Click" />
-            <asp:Button id="twitterSetUpButton" visible="false" runat="server" class="btn btn-primary btn-block" Text="Twitter Set Up" OnClick="twitterSetUpButton_Click" />
-        <!-- Modal -->
-        <!--<div id="credentialPopup" class="modal fade" role="dialog">
-            <credential-popup></credential-popup>
-        </div>-->
-</div>
+    <div id="userArea">
+        <div id="goalArea" runat="server" visible="false">
+            Goal Name:
+            <asp:DropDownList runat="server" ID="currentGoalDropDown"></asp:DropDownList>
+
+            <br />
+            <br />
+            <asp:Button ID="setupGoalButton" runat="server" Text="New Goal" OnClick="setupGoalButton_Click" />
+            <br />
+            <br />
+            <div id="newGoalArea" runat="server" visible="false">
+                Goal Name:
+                <asp:TextBox runat="server" ID="goalName"></asp:TextBox>
+                Goal Start Date:
+                <asp:Button runat="server" ID="startDateButton" Text="Not Set"></asp:Button>
+                Goal End Date:
+                <asp:Button runat="server" ID="endDateButton" Text="Not Set"></asp:Button>
+                <div runat="server" id="calendarArea" visible="false">
+                    <br />
+                    <asp:Calendar runat="server" ID="calendar"></asp:Calendar>
+                    <br />
+                    <asp:Button runat="server" ID="setCalendarDate" Text="Set Date"></asp:Button>
+                    <asp:Label runat="server" ID="calendarErrorLabel" Visible="false"></asp:Label>
+                </div>
+                <br />
+                <br />
+                <asp:Table runat="server" ID="newGoalGrid">
+                    <asp:TableHeaderRow>
+                        <asp:TableHeaderCell>
+                        Channel
+                        </asp:TableHeaderCell>
+                        <asp:TableHeaderCell>
+                        Exposure
+                        </asp:TableHeaderCell>
+                        <asp:TableHeaderCell>
+                        Influence
+                        </asp:TableHeaderCell>
+                    </asp:TableHeaderRow>
+                    <asp:TableRow>
+                        <asp:TableCell>
+                        Overall
+                        </asp:TableCell>
+                        <asp:TableCell>
+                            <asp:Label runat="server" ID="overallExposure" Text="0"></asp:Label>
+                        </asp:TableCell>
+                        <asp:TableCell>
+                            <asp:Label runat="server" ID="overallInfluence" Text="0"></asp:Label>
+                        </asp:TableCell>
+                    </asp:TableRow>
+                </asp:Table>
+                <br />
+                <br />
+                <asp:Button ID="newGoalButton" runat="server" Text="Set Goal" />
+            </div>
+        </div>
+        <br />
+        <br />
+        <asp:Button ID="facebookSetUpButton" Visible="false" runat="server" class="btn btn-primary btn-block" Text="Facebook Set Up" OnClick="facebookSetUpButton_Click" />
+        <asp:Button ID="twitterSetUpButton" Visible="false" runat="server" class="btn btn-primary btn-block" Text="Twitter Set Up" OnClick="twitterSetUpButton_Click" />
+    </div>
 </asp:Content>
--- FDM90/Pages/Content/Home.aspx.cs
+++ FDM90/Pages/Content/Home.aspx.cs
@@ -1,4 +1,5 @@
-﻿using FDM90.Singleton;
+﻿using FDM90.Models;
+using FDM90.Singleton;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,62 @@ namespace FDM90.Pages.Content
 {
     public partial class Home : System.Web.UI.Page
     {
+        private string[] metrics = { "Exposure", "Influence" };
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(UserSingleton.Instance.CurrentUser != null)
+            if (UserSingleton.Instance.CurrentUser != null)
             {
                 facebookSetUpButton.Visible = !UserSingleton.Instance.CurrentUser.Facebook;
                 twitterSetUpButton.Visible = !UserSingleton.Instance.CurrentUser.Twitter;
+                goalArea.Visible = true;
+                string[] userGoals = null;
+                currentGoalDropDown.DataSource = userGoals?.Count() > 0 ? userGoals : new string[] { "No Current Goals" };
+                SetUpTableControls();
+                startDateButton.Click += new EventHandler(StartCalendar);
+                endDateButton.Click += new EventHandler(StartCalendar);
+                setCalendarDate.Click += new EventHandler(SetDate);
             }
+        }
+
+        protected void StartCalendar(object sender, EventArgs e)
+        {
+            DateTime setDate = new DateTime();
+            calendar.SelectedDate = DateTime.TryParse(((Button)sender).Text, out setDate) ? setDate : calendar.TodaysDate;
+
+            ((Button)sender).Text = "Setting...";
+
+            calendarArea.Visible = true;
+        }
+
+        protected void SetDate(object sender, EventArgs e)
+        {
+            calendarErrorLabel.Visible = false;
+            calendarErrorLabel.Text = string.Empty;
+            if (startDateButton.Text == "Setting...")
+            {
+                if (calendar.SelectedDate > calendar.TodaysDate.AddDays(-30))
+                {
+                    startDateButton.Text = calendar.SelectedDate.ToShortDateString();
+                }
+                else
+                {
+                    calendarErrorLabel.Visible = true;
+                    calendarErrorLabel.Text = "Start date must be within the past 30 days";
+                }
+            }
+            else
+            {
+                if (calendar.SelectedDate < DateTime.Parse(startDateButton.Text))
+                {
+                    endDateButton.Text = calendar.SelectedDate.ToShortDateString();
+                }
+                else
+                {
+                    calendarErrorLabel.Visible = true;
+                    calendarErrorLabel.Text = "End date must be greater than start date";
+                }
+            }
+            calendarArea.Visible = !string.IsNullOrWhiteSpace(calendarErrorLabel.Text);
         }
 
         protected void facebookSetUpButton_Click(object sender, EventArgs e)
@@ -28,5 +78,55 @@ namespace FDM90.Pages.Content
             Response.Redirect("Twitter.aspx");
         }
 
+        private void SetUpTableControls()
+        {
+            foreach (string media in UserSingleton.Instance.CurrentUser.GetType().GetProperties().Where(x =>
+                    x.PropertyType == typeof(bool) && bool.Parse(x.GetValue(UserSingleton.Instance.CurrentUser).ToString()))
+                        .Select(s => s.Name))
+            {
+                TableRow tableRow = new TableRow();
+                TableCell metricCell = new TableCell();
+                metricCell.Text = media;
+                tableRow.Cells.Add(metricCell);
+
+                foreach (string metric in metrics)
+                {
+                    TableCell mediaCell = new TableCell();
+                    TextBox textBox = new TextBox();
+                    textBox.ID = media + metric;
+                    textBox.TextChanged += new EventHandler(textBox_Changed);
+                    textBox.Text = "0";
+                    mediaCell.Controls.Add(textBox);
+
+                    tableRow.Cells.Add(mediaCell);
+                }
+
+                newGoalGrid.Rows.Add(tableRow);
+            }
+        }
+
+        protected void setupGoalButton_Click(object sender, EventArgs e)
+        {
+            newGoalArea.Visible = true;
+        }
+
+        protected void textBox_Changed(object sender, EventArgs e)
+        {
+            string metricSender = ((TextBox)sender).ID;
+
+            foreach(string metric in metrics.Where(w => metricSender.Contains(w)))
+            {
+                int runningTotal = 0;
+                foreach (string media in UserSingleton.Instance.CurrentUser.GetType().GetProperties().Where(x =>
+                     x.PropertyType == typeof(bool) && bool.Parse(x.GetValue(UserSingleton.Instance.CurrentUser).ToString()))
+                        .Select(s => s.Name))
+                {
+                    runningTotal += int.Parse(((TextBox)newGoalGrid.FindControl(media + metric)).Text);
+                }
+
+                var overallControl = (Label)newGoalGrid.FindControl("overall" + metric);
+                overallControl.Text = runningTotal.ToString();
+            }
+        }
     }
 }
