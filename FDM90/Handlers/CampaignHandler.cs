cfde77f8f981cff893aa4a68a566dca0b22a1de0
owner <owner@HP>
2017-09-10 16:30:43 +0100

    update for campaign handler to generate datatable for campaigns

--- FDM90/Handlers/CampaignHandler.cs
+++ FDM90/Handlers/CampaignHandler.cs
@@ -8,6 +8,7 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Data;
 
 namespace FDM90.Handlers
 {
@@ -244,5 +245,83 @@ namespace FDM90.Handlers
         {
             return _campaignReadMultipleRepo.ReadMultipleSpecific(userId.ToString()).ToList();
         }
-    }
+
+        public DataTable GenerateCampaignDataTable(Campaign campaign)
+        {
+            DataTable campaignDataTable = new DataTable();
+            campaignDataTable.Columns.Add("Source", typeof(string));
+            campaignDataTable.Columns.Add("Week", typeof(string));
+            campaignDataTable.Columns.Add("Metric", typeof(string));
+            campaignDataTable.Columns.Add("Target", typeof(int));
+            campaignDataTable.Columns.Add("Progress", typeof(int));
+            campaignDataTable.Columns.Add("AccumulatedProgress", typeof(int));
+            JObject progress = JObject.Parse(campaign.Progress);
+            JObject target = JObject.Parse(campaign.Targets);
+
+            foreach (JProperty media in progress.Children())
+            {
+                foreach (JProperty week in media.Values().OrderBy(o => o.Path.Substring(4)))
+                {
+                    foreach (JProperty metric in week.Values())
+                    {
+                        DataRow row = campaignDataTable.NewRow();
+                        row[0] = media.Name;
+                        row[1] = week.Name.Substring(4);
+                        row[2] = metric.Name;
+                        row[3] = target[media.Name][metric.Name];
+                        row[4] = metric.Value;
+
+                        if (campaignDataTable.AsEnumerable().Where(w => w[0].ToString() == media.Name && w[2].ToString() == metric.Name).Count() == 0)
+                        {
+                            row[5] = metric.Value;
+                        }
+                        else
+                        {
+                            row[5] = int.Parse(metric.Value.ToString())
+                                        + int.Parse(campaignDataTable.AsEnumerable().Where(w => w[0].ToString() == media.Name && w[2].ToString() == metric.Name).Last()[5].ToString());
+                        }
+
+                        campaignDataTable.Rows.Add(row);
+                    }
+                }
+            }
+
+            foreach (var groupRow in campaignDataTable.AsEnumerable().GroupBy(g => new { Week = g[1], Metric = g[2] }).OrderBy(o => o.Key.Week))
+            {
+                DataRow row = campaignDataTable.NewRow();
+                row[0] = "Overall";
+                row[1] = groupRow.First()[1];
+                row[2] = groupRow.First()[2];
+
+                int accumulatedTarget = 0;
+
+                foreach (JProperty value in target.Children())
+                {
+                    accumulatedTarget += int.Parse(value.Value[groupRow.First()[2]].ToString());
+                }
+
+                row[3] = accumulatedTarget;
+
+                row[4] = groupRow.Sum(x => int.Parse(x[4].ToString()));
+
+                var overallEntry = campaignDataTable.AsEnumerable().Where(w => w[0].ToString() == "Overall"
+                                        && w[2].ToString() == groupRow.First()[2].ToString()
+                                            && w[1].ToString() == (int.Parse(groupRow.First()[1].ToString()) - 1).ToString());
+
+                if (overallEntry.Count() != 0 && (int)overallEntry.First()[5] > groupRow.Sum(x => int.Parse(x[5].ToString())))
+                {
+                    row[5] = (int)overallEntry.First()[5] + groupRow.Sum(x => int.Parse(x[5].ToString()));
+                }
+                else
+                {
+                    row[5] = groupRow.Sum(x => int.Parse(x[5].ToString()));
+                }
+
+                campaignDataTable.Rows.Add(row);
+
+            }
+
+            return campaignDataTable;
+        }
+        }
 }
