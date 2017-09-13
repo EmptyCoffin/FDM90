4d8a99b790fd5ae0da8fd04d0fc4b7ba84ff8d0d
owner <owner@HP>
2017-09-13 22:06:14 +0100

    Helper class unit tests

---
+++ FDM90UnitTests/DateHelperUnitTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FDM90.Models.Helpers;
+using System.Linq;
+
+namespace FDM90UnitTests
+{
+    [TestClass]
+    public class DateHelperUnitTests
+    {
+        int endDateDifference = -1;
+        int startDateDifference = -1;
+        DateTime startDate;
+        DateTime endDate;
+
+        [TestInitialize]
+        public void StartUp()
+        {
+            endDateDifference = 3;
+            startDateDifference = 15;
+            startDate = DateTime.Now.AddDays(-startDateDifference);
+            endDate = DateTime.Now.AddDays(-endDateDifference);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            endDateDifference = -1;
+            startDateDifference = -1;
+            startDate = new DateTime();
+            endDate = new DateTime();
+        }
+
+        [TestMethod]
+        public void GetDates_GivenDateRangeImplicitIncludeCurrentWeek_ReturnsTrueIfValuesIncludeCurrentWeek()
+        {
+            // arrange
+
+            // act
+            var result = DateHelper.GetDates(startDate, endDate);
+
+            // assert
+            Assert.AreEqual(startDateDifference - endDateDifference + 1, result.Count());
+
+            for (int i = endDateDifference; i < startDateDifference; i++)
+            {
+                Assert.IsTrue(result.Select(s => s.Date).Contains(DateTime.Now.AddDays(-i).Date));
+            }
+        }
+
+        [TestMethod]
+        public void GetDates_GivenDateRangeExplicitIncludeCurrentWeek_ReturnsTrueIfValuesIncludeCurrentWeek()
+        {
+            // arrange
+
+            // act
+            var result = DateHelper.GetDates(startDate, endDate, true);
+
+            // assert
+            Assert.AreEqual(startDateDifference - endDateDifference + 1, result.Count());
+
+            for (int i = endDateDifference; i < startDateDifference; i++)
+            {
+                Assert.IsTrue(result.Select(s => s.Date).Contains(DateTime.Now.AddDays(-i).Date));
+            }
+        }
+
+        [TestMethod]
+        public void GetDates_GivenDateRangeAndNotIncludeCurrentWeek_ReturnsTrueIfValuesDontCurrentWeek()
+        {
+            // arrange
+
+            // act
+            var result = DateHelper.GetDates(startDate, endDate, false);
+
+            // assert
+            Assert.AreEqual(startDateDifference - 7, result.Count());
+
+            for (int i = endDateDifference; i < startDateDifference; i++)
+            {
+                if (i <= 7)
+                {
+                    Assert.IsFalse(result.Select(s => s.Date).Contains(DateTime.Now.AddDays(-i).Date));
+                }
+                else
+                {
+                    Assert.IsTrue(result.Select(s => s.Date).Contains(DateTime.Now.AddDays(-i).Date));
+                }
+            }
+        }
+    }
+}
---
+++ FDM90UnitTests/FacebookHelperUnitTests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FDM90.Models.Helpers;
+
+namespace FDM90UnitTests
+{
+    [TestClass]
+    public class FacebookHelperUnitTests
+    {
+        string testId = string.Empty;
+        string[] fields = null;
+
+        [TestInitialize]
+        public void StartUp()
+        {
+            testId = "123456789";
+            fields = new string[0];
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            testId = null;
+            fields = null;
+        }
+
+        [TestMethod]
+        public void UrlBuilder_GivenIdParameter_ReturnsTrueIfUrlContainsIdParameterAndValue()
+        {
+            // arrange
+            fields = new[] { FacebookHelper.AccessToken, FacebookHelper.Id, FacebookHelper.Name };
+
+            // act
+            var result = FacebookHelper.UrlBuilder(FacebookParameters.Id, testId, fields);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains(FacebookHelper.BaseUrl));
+            Assert.IsTrue(result.Contains(FacebookHelper.IdParameter));
+            Assert.IsFalse(result.Contains(testId));
+
+            foreach (string field in fields)
+            {
+                Assert.IsTrue(result.Contains(field));
+            }
+        }
+
+        [TestMethod]
+        public void UrlBuilder_GivenFieldParameter_ReturnsTrueIfUrlContainsFieldParameterAndValue()
+        {
+            // arrange
+            fields = new[] { FacebookHelper.FanCount, FacebookHelper.TalkingAboutCount, FacebookHelper.PageFansCity };
+
+            // act
+            var result = FacebookHelper.UrlBuilder(FacebookParameters.Field, testId, fields);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains(FacebookHelper.BaseUrl));
+            Assert.IsTrue(result.Contains(FacebookHelper.FieldParameter));
+            Assert.IsFalse(result.Contains(testId));
+
+            foreach (string field in fields)
+            {
+                Assert.IsTrue(result.Contains(field));
+            }
+        }
+
+        [TestMethod]
+        public void UrlBuilder_GivenInsightParameterWithoutIdParameter_ReturnsTrueIfUrlContainsInsightAndIdParameterAndValue()
+        {
+            // arrange
+            fields = new[] { FacebookHelper.Posts, FacebookHelper.PostReach, FacebookHelper.PostNegativity };
+
+            // act
+            var result = FacebookHelper.UrlBuilder(FacebookParameters.Insight, string.Empty, fields);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains(FacebookHelper.BaseUrl));
+            Assert.IsTrue(result.Contains(FacebookHelper.InsightParameter));
+            Assert.IsFalse(result.Contains(testId));
+
+            foreach (string field in fields)
+            {
+                Assert.IsTrue(result.Contains(field));
+            }
+        }
+
+        [TestMethod]
+        public void UrlBuilder_GivenInsightParameterWithIdParameter_ReturnsTrueIfUrlContainsInsightParameterAndValue()
+        {
+            // arrange
+            fields = new[] { FacebookHelper.Posts, FacebookHelper.PostReach, FacebookHelper.PostNegativity };
+
+            // act
+            var result = FacebookHelper.UrlBuilder(FacebookParameters.Insight, testId, fields);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains(FacebookHelper.BaseUrl));
+            Assert.IsTrue(result.Contains(FacebookHelper.InsightParameter));
+            Assert.IsTrue(result.Contains(FacebookHelper.InsightIdParameter));
+            Assert.IsTrue(result.Contains(testId));
+
+            foreach (string field in fields)
+            {
+                Assert.IsTrue(result.Contains(field));
+            }
+        }
+
+        [TestMethod]
+        public void UrlBuilder_GivenAccountParameter_ReturnsTrueIfUrlContainsAccountParameterAndValue()
+        {
+            // arrange
+            fields = new[] { FacebookHelper.Posts, FacebookHelper.PostReach, FacebookHelper.PostNegativity };
+
+            // act
+            var result = FacebookHelper.UrlBuilder(FacebookParameters.Account, testId, fields);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains(FacebookHelper.BaseUrl));
+            Assert.IsTrue(result.Contains(FacebookHelper.AccountParameter));
+            Assert.IsTrue(result.Contains(testId));
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                if (i > 0)
+                {
+                    Assert.IsFalse(result.Contains(fields[i]));
+                }
+                else
+                {
+                    Assert.IsTrue(result.Contains(fields[i]));
+                }
+            }
+        }
+    }
+}
